92277c30e432e40ec9dc961f4b56ef73e54b5967
nicohlr <nicolas.houlier@gmail.com>
2020-05-25 14:59:34 +0200

    add complete favicon set for doc

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -4,7 +4,15 @@ module.exports = ctx => ({
     description: 'A Jupyter - Chart.js bridge enabling interactive data visualization in the Jupyter notebook.',
     //base: '/<REPO>/', 
     head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }]
+        ['link', {rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png'}],
+        ['link', {rel: 'icon', type:'image/png', sizes: '192x192', href: '/android-chrome-192x192.png'}],
+        ['link', {rel: 'icon', type:'image/png', sizes: '32x32', href: '/favicon-32x32.png'}],
+        ['link', {rel: 'icon', type:'image/png', sizes: '16x16', href: '/favicon-16x16.png'}],
+        ['link', {rel: 'manifest', href: '/site.webmanifest'}],
+        ['link', {rel: 'mask-icon', href: '/safari-pinned-tab.svg', color:"#36a3ec"}],
+        ['meta', {name: 'msapplication-TileImage', content: '/mstile-150x150.png'}],
+        ['meta', {name: 'msapplication-TileColor', content: '#2b5797'}],
+        ['meta', {name: 'theme-color', content: '#ffffff'}],
     ],
 
     themeConfig: {
