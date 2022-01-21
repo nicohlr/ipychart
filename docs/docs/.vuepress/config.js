18f424acdbe6364e24ce215d8808c39a5c7337fb
Nicolas Houlier <nicolas.houlier@gmail.com>
2022-01-21 11:38:55 +0100

    Doc: Include and adapt the chartjs vuepress theme

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -15,7 +15,6 @@ export default ctx => ({
         ['meta', {name: 'msapplication-TileColor', content: '#2b5797'}],
         ['meta', {name: 'theme-color', content: '#ffffff'}],
     ],
-	theme: 'chartjs',
 	plugins: [
 		['flexsearch', {maxSuggestions: 8}]
 	],
